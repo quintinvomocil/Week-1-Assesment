652dab96cf0a5ad2b5d7446d7f0d8605ed07314d
Quintin Vomocil <quintinvomocil@gmail.com>
2022-06-18 14:09:05 -0600

    Finished all Qs in arrays.js and the full assessment

--- arrays.js
+++ arrays.js
@@ -5,10 +5,19 @@ const faveColors = ['red', 'green', 'black']
 
 //CODE HERE
 
+let colorCopy = faveColors.slice(0, 3)
+ 
+//console.log(colorCopy)
+
+
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
 //CODE HERE
+
+colorCopy.push("Chartreuse");
+//console.log(colorCopy);
+
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -19,6 +28,11 @@ const faveColors = ['red', 'green', 'black']
 const numbers = [1, 2, 3, 4, 5]
 
 //CODE HERE
+
+let middleNums = numbers.slice(1, 4)
+console.log(middleNums)
+
+
 
 
 //////////////////PROBLEM 4////////////////////
@@ -34,3 +48,15 @@ const numbers = [1, 2, 3, 4, 5]
 const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
 
 // CODE HERE
+
+let answers = [];
+
+for (let i = 0; i < bigOrSmallArray.length; i++) {
+    if (bigOrSmallArray[i] > 100) {
+        answers.push('big')
+    } else if (bigOrSmallArray[i] <= 100) {
+      answers.push('small')
+    }
+  }
+  console.log(answers)
+  
